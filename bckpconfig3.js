7feb898d7dce3434c4e3751eeb892021f3fbf799
yanrn12 <107698772+yanrn12@users.noreply.github.com>
2022-07-22 01:10:11 +0700

    Update bckpconfig3.js

--- bckpconfig3.js
+++ bckpconfig3.js
@@ -1,11 +1,11 @@
-gc1 = 'https://chat.whatsapp.com/JEDQfzMGZan4HA98wtxsNL'
+gc1 = ''
 gc2 = ''
 gc3 = ''
-global.linkGC = ['https://chat.whats', 'https://chat.whatsapp.com/JEDQfzMGZan4HA98wtxsNL'] // ganti jadi group lu
-global.owner = ['6282221792667','6282320830684'] // Put your number here //owner eval
-global.kontak = ['6282221792667','6282320830684'] //Ketika ada yang ngetik #owner
+global.linkGC = ['https://chat.whats', ''] // ganti jadi group lu
+global.owner = ['6289516947204','62881023285067'] // Put your number here //owner eval
+global.kontak = ['6289516947204','62881023285067'] //Ketika ada yang ngetik #owner
 global.mods = ['0'] // Want some help?
-global.prems = ['6282221792667','923021206454'] // Premium user has unlimited limit
+global.prems = ['6289516947204','62881023285067'] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com', 
@@ -41,20 +41,20 @@ xteamkey = 'cristian9407'
 namaig = 'GAK ADA :)'
 namagithub = 'GAK ADA :)'
 kasihcaption = `Done`
-namakontak1 = 'Owner Tio'
+namakontak1 = 'Owner 𝚒𝚢𝚊𝚗Ⓒ︎'
 namakontak2 = 'Nomor Bot'
 
 //kasihcaption = `Done`
 
 // Sticker WM
-global.packname = 'BOTCAHX' // ganti aja
-global.author = 'BOTCAHX' // ganti aja
+global.packname = '𝚒𝚢𝚊𝚗Ⓒ︎' // ganti aja
+global.author = '𝚒𝚢𝚊𝚗Ⓒ︎' // ganti aja
 
 //yyy
-bc = 'BCH' //BOTCAHX BROADCAST
-footer = '\n© BOTCAHX'
-namabot = '𝘽𝙊𝙏𝘾𝘼𝙃𝙓'
-namalu = 'Tio'
+bc = 'BCH' //𝚒𝚢𝚊𝚗Ⓒ︎ BROADCAST
+footer = '\n© 𝚒𝚢𝚊𝚗Ⓒ︎'
+namabot = '𝚒𝚢𝚊𝚗Ⓒ︎'
+namalu = '𝚒𝚢𝚊𝚗Ⓒ︎'
 
 
 // 
