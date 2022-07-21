5db0f26c3fc468047bd48e9bb23ad1bf1491c4be
yanrn12 <107698772+yanrn12@users.noreply.github.com>
2022-07-22 01:01:02 +0700

    Update and rename plugins/donasi.js to plugin/donasi.js

--- plugin/donasi.js
+++ plugin/donasi.js
@@ -1,8 +1,8 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(fla + 'donasi')).buffer(), `
 ┌〔 Donasi • Emoney 〕
-├ PULSA : 081395861695
-├ DANA : 082221792667
+├ PULSA : 089516947204
+├ DANA : 0881023285067
 └────
 `.trim(), footer, 'Owner', '.owner')
 handler.help = ['donasi']
